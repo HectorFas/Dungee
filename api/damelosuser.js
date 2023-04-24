8043b0155402a53220ab21b5e9b5440c4bec16ff
HectorFas <114946996+HectorFas@users.noreply.github.com>
2023-04-24 16:36:19 +0200

    .

---
+++ api/damelosuser.js
@@ -0,0 +1,5 @@
+export default function handler(request, response) {
+    fetch(`https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app/.vercel.app//`)
+    .then(eljsoncomostring => response.send(eljsonperocomstring))
+    
+}
