245fd1755b9383530c0c78cb448580d4f1f03e53
HectorFas <114946996+HectorFas@users.noreply.github.com>
2023-04-25 18:03:57 +0200

    .

--- api/damelaspreguntas.js
+++ api/damelaspreguntas.js
@@ -1,9 +1,9 @@
 export default function hadler(request, response) {
     const questions = {
         "preguntas" :[
-         {pregunta : ["¿Cuanto es dos más dos?", "2",false,"3",false,"4",true ]},
-         {pregunta : ["¿Cuanto es dos más tres?", "2",false,"5",true,"4",false ]},  
-         {pregunta : ["¿Cuanto es cinco más cinco?", "10",true,"3",false,"4",false ]}, 
+         {pregunta1 : ["¿Cuanto es dos más dos?", "2",false,"3",false,"4",true ]},
+         {pregunta2 : ["¿Cuanto es dos más tres?", "2",false,"5",true,"4",false ]},  
+         {pregunta3 : ["¿Cuanto es cinco más cinco?", "10",true,"3",false,"4",false ]}, 
         ] 
 
     }
