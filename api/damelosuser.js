1cd2df6ff5e911d3f35433dc85a7215a467f4c3e
HectorFas <114946996+HectorFas@users.noreply.github.com>
2023-04-25 15:26:19 +0200

    .

--- api/damelosuser.js
+++ api/damelosuser.js
@@ -1,5 +1,6 @@
 export default function handler(request, response) {
-    fetch(`https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app/pins.json`)
+    let pin = request.query.pin
+    fetch(`https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}.json`)
     .then(datosstring => datosstring.json())
     .then(eljsondearriba => response.json(eljsondearriba))
     
