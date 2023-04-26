0fb29d198d490a9ce3867ef3e56c756bd5761ee5
HectorFas <114946996+HectorFas@users.noreply.github.com>
2023-04-26 15:58:49 +0200

    .

--- api/damelaspreguntas.js
+++ api/damelaspreguntas.js
@@ -4,7 +4,32 @@ export default function hadler(request, response) {
          ["¿Cuanto es dos más dos?", "2",false,"3",false,"4",true ],   //Indicar respuesta correcta mdiante posicion del array.
          ["¿Cuanto es dos más tres?", "2",false,"5",true,"4",false ],  
          ["¿Cuanto es cinco más cinco?", "10",true,"3",false,"4",false ]
-        ] 
+        ] ,
+
+
+        "preguntas" : {
+            "Pregunta" : "2+1?",
+            "Res1" : "2",
+            "Res2" : "3",
+            "Res3" : "5",
+            "Corr" : "3"
+        },
+        "pregunta2" : {
+            "pregunta" : "2+2",
+            "Res1" : "4",
+            "Res2" : "3",
+            "Res3" : "5",
+            "Corr" : "4"
+
+        },
+        "pregunta3" : {
+            "pregunta" : "10+9",
+            "Res1" : "9",
+            "Res2" : "30",
+            "Res3" : "19",
+            "Corr" : "19"
+
+        }
 
     }
 
