68679798be1e43ae11e12ecf7b62b37f4ff65182
yo y tu <yo@tuy>
2023-05-23 14:52:53 +0200

    .

--- modules/bd.js
+++ modules/bd.js
@@ -17,6 +17,14 @@ export function upDatePreguntaActual(i, pin) {
     });
 }
 
+export function addUser(pin,nom){
+    return fetch(`https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}/users.json`, {
+            method: 'POST',
+            body: `{"name": "${nom}"}`
+        })
+        .then(data => data.json())
+}
+
 export function getPreguntaActual(pin){
     return fetch(`${dburl}/pins/pin${pin}/pregunta.json`)
             .then(data => data.json())  //Cogemos el json preguntas del fetch(descargar) y de ahi como esos datos nos los da como String en JSON.
