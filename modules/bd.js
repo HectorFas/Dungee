ea5d4a84070f41013bdcf2ef963bebeacf8906f5
yo y tu <yo@tuy>
2023-05-23 14:37:05 +0200

    .

---
+++ modules/bd.js
@@ -0,0 +1,31 @@
+export function fetchUsuarios(pin) {
+    return fetch(`/api/damelosuser?pin=${pin}`)
+            .then(datos => datos.json())
+}
+
+export function fetchPreguntas(pin) {
+    return fetch(`/api/damelaspreguntas`)
+        .then(datos => datos.json())
+}
+
+export function upDatePreguntaActual(i, pin) {
+    fetch(`https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}/pregunta.json`, {
+        method: 'PUT',
+        body: `{"valor": ${i}}`
+    });
+}
+
+export function getPreguntaAcual(pin) {
+
+    return fetch(`https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}/pregunta.json`)
+        .then(data => data.json())  //Cogemos el json preguntas del fetch(descargar) y de ahi como esos datos nos los da como String en JSON.
+}
+
+
+export function upDateRespuesta(pin, userid, preguntaActual, respuesta) {
+    console.log(`ReSPONDO A LA PREGUNTA ${preguntaActual} LA ${respuesta}`)
+      fetch(`https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}/users/${userid}/respuestas/pregunta${preguntaActual}.json`, {
+            method: 'PUT',
+            body: `{"letra" : "${respuesta}"}`
+        })
+}
