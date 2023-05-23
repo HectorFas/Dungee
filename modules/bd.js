d755f6b2af981b6ee3f523f89f4c7730a82c1363
yo y tu <yo@tuy>
2023-05-23 14:50:54 +0200

    .

--- modules/bd.js
+++ modules/bd.js
@@ -1,3 +1,5 @@
+const dburl = "https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app";
+
 export function fetchUsuarios(pin) {
     return fetch(`/api/damelosuser?pin=${pin}`)
             .then(datos => datos.json())
@@ -9,20 +11,20 @@ export function fetchPreguntas() {
 }
 
 export function upDatePreguntaActual(i, pin) {
-    fetch(`https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}/pregunta.json`, {
+    fetch(`${dburl}/pins/pin${pin}/pregunta.json`, {
         method: 'PUT',
         body: `{"valor": ${i}}`
     });
 }
 
 export function getPreguntaActual(pin){
-    return fetch(`https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}/pregunta.json`)
+    return fetch(`${dburl}/pins/pin${pin}/pregunta.json`)
             .then(data => data.json())  //Cogemos el json preguntas del fetch(descargar) y de ahi como esos datos nos los da como String en JSON.
 }
 
 
 export function actualizarRespuesta(pin, userid, preguntaActual, respuesta){
-    fetch(`https://dungee-431f9-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}/users/${userid}/respuestas/pregunta${preguntaActual}.json`, {
+    fetch(`${dburl}/pins/pin${pin}/users/${userid}/respuestas/pregunta${preguntaActual}.json`, {
             method: 'PUT',
             body: `{"letra" : "${respuesta}"}`
     })
