693f20a459624cff2256e04392a943f888ea5557
yo y tu <yo@tuy>
2023-05-25 19:35:12 +0200

    .

--- modules/ui.js
+++ modules/ui.js
@@ -2,3 +2,9 @@ export function mostrar(elementId, className) {
     document.querySelectorAll(`.${className}`).forEach(el => el.classList.add("oculto"))
     document.getElementById(elementId).classList.remove("oculto")
 }
+
+export function ocultar(elementId, className) {
+    document.querySelectorAll(`.${className}`).forEach(el => el.classList.add("oculto"))
+    document.getElementById(elementId).classList.add("oculto")
+}
+
